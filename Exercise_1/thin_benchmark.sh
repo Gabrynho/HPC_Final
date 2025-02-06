7767bfc84693e1b1d578a35b6ef5575d8975fe92
Gabrynho <masellagabriel@gmail.com>
2025-02-06 22:23:48 +0100

    Output results to CSV and code fixed

--- Exercise_1/thin_benchmark.sh
+++ Exercise_1/thin_benchmark.sh
@@ -10,24 +10,35 @@
 
 module load openMPI/4.1.6/gnu/14.2.1
 
+# Set the CSV output file and write header line
+CSV_OUT="results.csv"
+echo "Type,Algorithm,NP,Size,Avg_Latency,Min_Latency,Max_Latency,Iterations" > "${CSV_OUT}"
+
 # Broadcast Benchmark
-for np in {1..24}; do
+for NP in {1..24}; do
   for ALG in 1 2 3 5; do  # 1: basic linear, 2: chain, 3: pipeline, 5: binary tree
-    mpirun --map-by core \
+    echo "Running Broadcast: NP=${NP}, ALG=${ALG}"
+    result=$(mpirun --map-by core -n "${NP}" \
       --mca coll_tuned_use_dynamic_rules true \
       --mca coll_tuned_bcast_algorithm "${ALG}" \
-      ../osu-micro-benchmarks-7.5/c/mpi/collective/blocking/osu_bcast -i 1000 -x 100 \
-      -n "${np}" -m 64
+      osu-micro-benchmarks-7.5/c/mpi/collective/blocking/osu_bcast -i 1000 -x 100 \
+       -m 64:64 -f)
+
+    # Append lines that start with a number (data rows) to CSV file, prefixing with type, algorithm, NP
+    echo "$result" | awk -v type="Broadcast" -v alg="${ALG}" -v np="${NP}" '$1 ~ /^[0-9]+$/ {print type","alg","np","$1","$2","$3","$4","$5}' >> "${CSV_OUT}"
   done
 done
 
 # Reduce Benchmark
-for np in {1..24}; do
+for NP in {1..24}; do
   for ALG in 1 2 3 4; do  # 1: linear, 2: chain, 3: pipeline, 4: binary
-    mpirun --map-by core \
+    echo "Running Reduce: NP=${NP}, ALG=${ALG}"
+    result=$(mpirun --map-by core -n "${NP}" \
       --mca coll_tuned_use_dynamic_rules true \
       --mca coll_tuned_reduce_algorithm "${ALG}" \
-      ../osu-micro-benchmarks-7.5/c/mpi/collective/blocking/osu_allreduce -i 1000 -x 100 \
-      -n "${np}" -m 64
+      osu-micro-benchmarks-7.5/c/mpi/collective/blocking/osu_allreduce -i 1000 -x 100 \
+      -m 64:64 -f)
+
+    echo "$result" | awk -v type="Reduce" -v alg="${ALG}" -v np="${NP}" '$1 ~ /^[0-9]+$/ {print type","alg","np","$1","$2","$3","$4","$5}' >> "${CSV_OUT}"
   done
 done
